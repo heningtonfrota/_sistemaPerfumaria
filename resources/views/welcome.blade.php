d1bf5efec56bf01520195c1e4f3fe59d0724153c
Henington Frota <tom.frota@hotmail.com>
2021-04-04 10:50:03 -0400

    Configurações do projeto: Laravel, Inertia e VueJS

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fabrica de Perfumes</title>
+</head>
+<body>
+    <h1>Teste de Titulo</h1>
+</body>
+</html>
